5a232c5320cf0c7911c1d684d7b89452e88bebaa
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-02-09 18:15:46 -0300

    [Feat][Adiciona listarTodos no service de Filmes]

--- src/main/java/dev/movieflix/movieflix/Service/FilmeService.java
+++ src/main/java/dev/movieflix/movieflix/Service/FilmeService.java
@@ -1,7 +1,10 @@
 package dev.movieflix.movieflix.Service;
 
+import dev.movieflix.movieflix.Model.FilmeModel;
 import dev.movieflix.movieflix.Repository.FilmeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FilmeService {
@@ -9,5 +12,10 @@ public class FilmeService {
     private final FilmeRepository filmeRepository;
     public FilmeService(FilmeRepository filmeRepository) {
         this.filmeRepository = filmeRepository;
+    }
+
+
+    public List<FilmeModel> listarFilmes(){
+        return filmeRepository.findAll();
     }
 }
