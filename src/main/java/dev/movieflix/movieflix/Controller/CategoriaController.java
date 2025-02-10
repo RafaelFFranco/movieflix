c2aca3fb22285b22203d0c95d54b862c27f643c5
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-02-10 17:46:44 -0300

    [Fix][Altera a rota padrão do Controller de Categoria para /movieflix/categorias]

--- src/main/java/dev/movieflix/movieflix/Controller/CategoriaController.java
+++ src/main/java/dev/movieflix/movieflix/Controller/CategoriaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movieflix/categoria")
+@RequestMapping("/movieflix/categorias")
 public class CategoriaController {
 
     private final CategoriaService categoriaService;
