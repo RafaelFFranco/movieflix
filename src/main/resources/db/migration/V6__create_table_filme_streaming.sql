a145161b3ded42fa37cc21b11b7cf402ebf61f15
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-02-17 17:49:12 -0300

    [Feat][Adiciona tabela filme_categoria e filme_streaming para realizar a relação de N para N de filmes com categorias e filmes com streamings]

---
+++ src/main/resources/db/migration/V6__create_table_filme_streaming.sql
@@ -0,0 +1,7 @@
+CREATE TABLE filme_streaming
+(
+    filme_id     INTEGER,
+    streaming_id INTEGER,
+    CONSTRAINT fk_filme_streaming_filme FOREIGN KEY (filme_id) REFERENCES filme (id),
+    CONSTRAINT fk_filme_streaming_streaming FOREIGN KEY (streaming_id) REFERENCES streaming (id)
+);
