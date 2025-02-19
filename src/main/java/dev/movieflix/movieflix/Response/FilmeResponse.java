5e9a3d9d380be6956db68acb48a0c40a15255432
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-02-18 21:11:38 -0300

    [Fix][Altera atributos de FilmeReponse para incluir List<Categoriareponse> / List<StreamingResponse]

--- src/main/java/dev/movieflix/movieflix/Response/FilmeResponse.java
+++ src/main/java/dev/movieflix/movieflix/Response/FilmeResponse.java
@@ -4,7 +4,16 @@ import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
-public record FilmeResponse(Long id, String titulo, String descricao, LocalDate dataLancamento, double nota, LocalDateTime dataCriacao, LocalDateTime dataAtualizacao) {
+public record FilmeResponse(Long id,
+                            String titulo,
+                            String descricao,
+                            LocalDate dataLancamento,
+                            double nota,
+                            LocalDateTime dataCriacao,
+                            LocalDateTime dataAtualizacao,
+                            List<CategoriaResponse> categorias,
+                            List<StreamingResponse> streamings) {
 }
