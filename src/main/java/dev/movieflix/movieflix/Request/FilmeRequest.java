5eb96880b856d1643bdd7f70af24d95cf7bbf7f5
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-02-18 21:14:19 -0300

    [Fix][Altera atributos de FilmeReponse para incluir List<Categoriareponse> / List<StreamingResponse]

--- src/main/java/dev/movieflix/movieflix/Request/FilmeRequest.java
+++ src/main/java/dev/movieflix/movieflix/Request/FilmeRequest.java
@@ -1,6 +1,12 @@
 package dev.movieflix.movieflix.Request;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public record FilmeRequest(String titulo, String descricao, LocalDate dataLancamento, double nota) {
+public record FilmeRequest(String titulo,
+                           String descricao,
+                           LocalDate dataLancamento,
+                           double nota,
+                           List<Long> categorias,
+                           List<Long> streamings) {
 }
